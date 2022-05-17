330cf5747e2bd029691408c3b78ed1b2fbaac44c
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2022-05-17 17:08:38 +0500

    Fetched IMDB Data

--- LearningAtMyOwn/ScrapingIMDBWebsite/index.py
+++ LearningAtMyOwn/ScrapingIMDBWebsite/index.py
@@ -1,17 +1,45 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 try:
     source = requests.get('https://www.imdb.com/chart/top')
     source.raise_for_status()
 
-    soup = BeautifulSoup(source.text,'html.parser')
+    soup = BeautifulSoup(source.text, 'html.parser')
 
-    movies = soup.find('tbody',class_="lister-list").find_all('tr')
+    movies = soup.find('tbody', class_="lister-list").find_all('tr')
+
+    # Defining the arrays to store the data
+    nameArray = []
+    rankArray = []
+    yearArray = []
+    ratingArray = []
 
     for movie in movies:
-        name = movie.find('td',class_="titleColumn")
-    # print(movies[0].find('td',class_="titleColumn").text)
+        name = movie.find('td', class_="titleColumn").a.text
+        nameArray.append(name)
+        rank = movie.find('td', class_="titleColumn").get_text(strip=True).split('.')[0]
+        rankArray.append(rank)
+        year = movie.find('td', class_="titleColumn").span.text.strip('()')
+        yearArray.append(year)
+        rating = movie.find('td', class_="ratingColumn imdbRating").strong.text
+        ratingArray.append(rating)
+        # print(rating)
+
+    print("Rating Array : ", ratingArray)
+
+    # Writing data finally to csv file
+    df = pd.DataFrame(
+        {
+            "Rank": rankArray,
+            "Name": nameArray,
+            "Year": yearArray,
+            "Rating": ratingArray
+        }
+    )
+    df.to_csv("IMDBData.csv", index=True)
+
 
 except Exception as e:
     print(e)
---
+++ README.md
@@ -0,0 +1,3 @@
+# Web Scraping
+
+### Contains code snippets for scraping multiple websites.Learning through multiple online courses,blogs and videos.Its only about being familiar with html and python or javascript and you can do that easily.
