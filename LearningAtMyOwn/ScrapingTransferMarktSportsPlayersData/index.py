96d5ce426d1e7ca3bfa83ce4d53b725753aed6aa
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2022-05-18 09:18:09 +0500

    Working

--- LearningAtMyOwn/ScrapingTransferMarktSportsPlayersData/index.py
+++ LearningAtMyOwn/ScrapingTransferMarktSportsPlayersData/index.py
@@ -14,13 +14,36 @@ try:
     soup = BeautifulSoup(content,features="lxml")
     # print(soup)
 
-    movies = soup.find('table',class_="items").find('tbody').find_all('tr')
+    clubs_data = soup.find('table',class_="items").find('tbody').find_all('tr')
 
-    #print(movies[0])
+    #print(players_data[0])
 
-    for movie in movies:
-        name = movie.find('td', class_="hauptlink no-border-links").a.text
-        print(name)
+    # Defining Arrays to store data
+    club_nameArray = []
+    club_squadArray = []
+    club_ageArray = []
+    club_foreignersArray = []
+    club_marketValueArray = []
+    club_totalMarketValueArray = []
+
+    for club_data in clubs_data:
+        # Extracting the data
+        club_name = club_data.find('td', class_="hauptlink no-border-links").a.text
+        club_squad = club_data.find_all('td', class_="zentriert")[1].a.text
+        club_age = club_data.find_all('td', class_="zentriert")[2].text
+        # club_foreigners = movie.find('td', class_="hauptlink no-border-links").a.text
+        # club_marketValue = movie.find('td', class_="hauptlink no-border-links").a.text
+        # club_totalMarketValue = movie.find('td', class_="hauptlink no-border-links").a.text
+
+        # Appending the values to the arrays
+        # club_nameArray.append(club_name)
+        # club_squadArray.append(club_squad)
+        # club_ageArray.append(club_age)
+        # club_foreignersArray.append(club_foreigners)   
+        # club_marketValueArray.append(club_marketValue)
+        # club_totalMarketValueArray.append(club_totalMarketValue)
+
+        print(club_age)
         break
 
 
