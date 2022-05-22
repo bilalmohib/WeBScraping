284c8c05ac3def4a3082fda5088246daf70c642f
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2022-05-22 22:41:18 +0500

    Started With Node JS

--- LearningAtMyOwn/Navigating and moving with selenium/index.py
+++ LearningAtMyOwn/Navigating and moving with selenium/index.py
@@ -1,38 +1,46 @@
-from asyncio.windows_events import NULL
-from itertools import count
-from weakref import finalize
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import time
 
-driver = webdriver.Chrome(executable_path='C:/ChromeDriver/chromedriver.exe')
-url_to_scrap = "https://www.techwithtim.net/"
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+url_to_scrap = "https://www.transfermarkt.us/"
 driver.get(url_to_scrap)
 
-link = driver.find_element(By.LINK_TEXT, "Python Programming")
-link.click()
+myDynamicElement = driver.find_element(By.ID("login")).span.text.click()
+print(myDynamicElement)
+# myDynamicElement.click()
+time.sleep(5)
 
-try:
+# link = driver.find_element(By.PARTIAL_LINK_TEXT, 'United States')
+# link.click()
+
+# try:
     # Will wait for 10 seconds
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(
-            (By.LINK_TEXT, "Beginner Python Tutorials"))
-    )
-    element.click()
+    # myDynamicElement = driver.find_element(By.TAG_NAME,"a")
+    # print(len(myDynamicElement))
+    # time.sleep(5)
 
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(
-            (By.ID, "sow-button-19310003"))
-    )
-    element.click()
-except:
-    driver.quit()
+    # element = WebDriverWait(driver, 10).until(
+    #     EC.presence_of_element_located(
+    #         (By.ID, "sow-button-19310003"))
+    # )
+    # element.click()
+
+    # driver.back()
+    # driver.back()
+    # driver.back()
+    # driver.forward()
+    # driver.forward()
+# except:
+#     print("Error occured")
 
 
 # content = driver.page_source
