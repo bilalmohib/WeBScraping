5fe9d4b3ee576f9cc54854dd0a2e4a09a3039c94
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2022-05-23 19:38:35 +0500

    Mil gaya

--- FreecodecampWebScrapingTutorial/main.py
+++ FreecodecampWebScrapingTutorial/main.py
@@ -4,12 +4,17 @@
 from bs4 import BeautifulSoup
 
 # Reading Html file
-with open('index.html','r', encoding="utf8") as html_file:
+with open('check.html','r', encoding="utf8") as html_file:
     content = html_file.read()
 
     soup = BeautifulSoup(content,'lxml')
     # print(soup.prettify())
-    tags = soup.find('div')
+    tags = soup.findAll('li')
     # List all the h1 tags using findAll
-    tags = soup.findAll('p')
-    print(tags)
+    # tags = soup.findAll('p')
+    print(len(tags))
+    list_countries = []
+    for tag in tags:
+        list_countries.append(tag.span.text)
+        # print(tag.span.text)
+    print(list_countries)
--- FreecodecampWebScrapingTutorial/selenium_tutorial_freecodecamp/scrapingtarget.py
+++ FreecodecampWebScrapingTutorial/selenium_tutorial_freecodecamp/scrapingtarget.py
@@ -2,23 +2,47 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait as wait
 
 os.environ['PATH'] += r"C:/ChromeDriver"
 browser = webdriver.Chrome()
 browser.get("https://www.transfermarkt.us/")
 element = browser.find_element(By.XPATH, '//*[@id="schnellsuche"]/input[1]')
 element.send_keys("J1 League")
-element2 = browser.find_element(By.XPATH, '//*[@id="schnellsuche"]/input[2]')
-element2.click()
+time.sleep(5)
+
+# element2 = browser.find_element(By.XPATH, '//*[@id="main"]/header/div[3]/tm-quick-select-bar//div/tm-quick-select[1]/div/div[1]/strong/text()')
+# element2.click()
+
+button = wait(browser, 10).until(EC.presence_of_element_located(
+    (By.XPATH, '//*[@id="schnellsuche"]/input[2]')))
+button.click()
+
+# element2 = browser.find_element(By.XPATH, '//*[@id="schnellsuche"]/input[2]')
+# element2.click()
+
+####################In Use###################################
+# button = wait(browser, 10).until(EC.presence_of_element_located(
+#     (By.XPATH, '//*[@id="schnellsuche"]/input[2]')))
+# button.click()
+# time.sleep(5)
+# print(browser.current_url)
+####################In Use###################################
+
+# button2 = wait(browser, 10).until(EC.presence_of_element_located(
+#     (By.XPATH, '//*[@id="yw0"]/table/tbody/tr[1]/td[2]/a')))
+# button2.click()
+# time.sleep(5)
 # element3 = browser.find_element(
-#     By.XPATH, '//*[@id="content"]/div/div/table/tbody/tr/td[3]/a')
+#     By.XPATH, '//*[@id="main"]/header/div[3]/tm-quick-select-bar//div/tm-quick-select[1]/div/div/strong')
 # element3.click()
 
 # browser.back()
 # browser.forward()
 # browser.back()
 # browser.back()
-#Reached start again
+# Reached start again
 # print(element)
 time.sleep(10)
 # browser.close()
